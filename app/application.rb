4677904bd0020333e8e42f4abb879f12bbc620fd
sgrant0225 <sharnellgrant@gmail.com>
2020-05-01 21:40:36 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,25 +7,25 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path.match(/items/) #route 
       @@items.each do |item|
-        resp.write "#{item}\n"
+        resp.write "#{item}\n" #this list all the items in the browser
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@cart.empty?
+      if @@cart.empty?  
         resp.write "Your cart is empty"
       else @@cart.each do |c|
-        resp.write "#{c}\n"  
+        resp.write "#{c}\n"  #list out items in the cart
         end
       end
     elsif req.path.match(/add/)  
       search_item = req.params["item"]
       if @@items.include?(search_item)
-        @@cart << search_item
-        resp.write "added #{search_item}"
+        @@cart << search_item   #if the item search for is avaiable, place in the cart
+        resp.write "added #{search_item}" 
       else
         resp.write "We don't have that item"
       end    
